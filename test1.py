ec735776845533e05ddfd8535c6d11105d8801b2
galimasiwf <127606676+galimasiwf@users.noreply.github.com>
2026-03-21 06:31:03 +0530

    Added a new user to the repo

--- test1.py
+++ test1.py
@@ -1,2 +1,8 @@
 name = "Nithish"
-print " My Name is " + name
+print(" My Name is " + name)
+age = 20
+print(" My Age is " + str(age))
+height = 5.8
+print(" My Height is " + str(height) + " feet")
+is_student = True
+print(" Am I a student? " + str(is_student))
